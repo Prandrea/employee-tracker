15789caf8278eabc2a9bb2f3ead7895768562177
prandrea <prandrea@utexas.edu>
2022-06-08 14:48:06 -0500

    tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,17 @@ CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
 );
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+
+)
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use(require('./'));
-
-
-
-module.exports = router;
