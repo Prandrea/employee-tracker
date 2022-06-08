d593008bf3cf1be93e70ad14f2a77e150f832077
prandrea <prandrea@utexas.edu>
2022-06-08 14:54:29 -0500

    seeds employees, schema employees

--- db/schema.sql
+++ db/schema.sql
@@ -18,5 +18,15 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER NULL,
+  FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
